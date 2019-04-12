340bea7774653e715750bad460f068b5f98e2fca
22pereai <49483164+22pereai@users.noreply.github.com>
2019-04-12 10:46:56 -0400

    Moda Challenge Added (Again)
    Fixed Errors

---
+++ moda_challenge.py
@@ -0,0 +1,32 @@
+# Aidan Perez
+# 4/10/19
+
+# Moda Challenge
+
+# Performs the calculations required by the moda challenge.
+
+x = 1
+while x <= 10:
+    print("Hello, world")
+    x += 1
+
+y = 0
+while y <= 9:
+    print ("%d" % (y))
+    y += 1
+
+z = 1
+while z <= 10:
+    print("%d" % (z))
+    z += 1
+
+t = 0
+while t <= 20:
+    print ("%d" % (t))
+    t += 2
+
+a = 0
+while a <= 10:
+    b = (a * a)
+    print ("%d" % (b))
+    a += 1
