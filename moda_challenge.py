ad72d91325e9a76cd36eb3585f1c4778202da7f9
22pereai <49483164+22pereai@users.noreply.github.com>
2019-04-12 10:42:00 -0400

    Added moda_challenge.py
    ye

---
+++ moda_challenge.py
@@ -0,0 +1,32 @@
+# Aidan Perez
+# 4/10/19
+
+# Hello World
+
+# Prints ("Hello, world") to the screen.
+
+x = 1
+while x <= 10:
+    print("Hello, world")
+    x += 1
+
+y = 0
+while y <= 9:
+    print ("%d" % (y))
+    y += 1
+
+z = 1
+while z <= 10:
+    print("%d" % (z))
+    z += 1
+
+t = 0
+while t <= 20:
+    print ("%d" % (t))
+    t += 2
+
+a = 0
+while a <= 10:
+    b = (a * a)
+    print ("%d" % (b))
+    a += 1
